e11ce139f63507a34aae1abb7f793e754d312d48
pavanvasala-123 <120788440+pavanvasala-123@users.noreply.github.com>
2024-01-24 10:32:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-// let menu_bar = document.getElementById("menu_bar");
-// let wrong_bar = document.getElementById("wrong_bar");
-// let mobile_nav_menu = document.getElementById("mobile_nav_menu");
 
-// menu_bar.addEventListener("click", () => {
-//   mobile_nav_menu.classList.toggle("mobile-nav");
-//   wrong_bar.classList.toggle("menu-close-icon");
-//   menu_bar.style.display = "none";
-// });
-// wrong_bar.addEventListener("click", () => {
-//   mobile_nav_menu.classList.toggle("mobile-nav");
-//   wrong_bar.classList.toggle("menu-close-icon");
-//   menu_bar.style.display = "block";
-// });
 
 function toggleMenu() {
   var navList = document.getElementById("nav-list");
